5cda5548384942db99ca4adc2bb12d7e94f5582e
Swojak-A <swojak.a@gmail.com>
2017-12-26 23:13:27 +0100

    add main_menu.tscn & new fonts - IMPORTANT FIX

---
+++ main_menu.gd
@@ -0,0 +1,17 @@
+extends Panel
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _on_versus_pressed():
+		get_node("/root/global").goto_scene("res://scenes/pong.tscn")
+		#get_tree().change_scene("res://scenes/pong.tscn")
+
+func _on_quit_pressed():
+		get_tree().quit()
