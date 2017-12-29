eed86826120eb2c6461366ff339aa4a8f150e069
Swojak-A <swojak.a@gmail.com>
2017-12-29 18:22:42 +0100

    add menu visibility:ON by ui_accept key

--- main_menu.gd
+++ main_menu.gd
@@ -1,13 +1,14 @@
 extends Panel
 
 # class member variables go here, for example:
+var menu_on = false
 # var a = 2
 # var b = "textvar"
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	pass
+	self.set_process(true)
 
 func _on_versus_pressed():
 		get_node("/root/global").goto_scene("res://scenes/pong.tscn")
@@ -18,3 +19,8 @@ func _on_quit_pressed():
 
 func _on_test_button_pressed():
 		get_node("menu").show()
+
+func _process(delta):
+	if(Input.is_action_pressed("ui_accept")) and menu_on == false:
+		get_node("menu").show()
+	
